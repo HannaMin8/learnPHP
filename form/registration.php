926d7b76770a4f5e25a332828d87ce854dcbfc3a
Anna <annyamin1984@gmail.com>
2024-02-02 22:46:59 -0600

    the form is not reset where the field is entered correctly

--- form/registration.php
+++ form/registration.php
@@ -4,14 +4,17 @@ declare(strict_types=1);
 echo '<pre>';
 $errors = array();
 $name = $_POST['name'] ?? '';
+$surname = $_POST['surname'] ?? '';
 $email = $_POST['email'] ?? '';
 $password = $_POST['password'] ?? '';
 $repeatPassword = $_POST['repeatPassword'] ?? '';
+$aboutMe = $_POST['aboutMe'] ?? '';
 $age = $_POST['age'] ?? '';
 $color = $_POST['color'] ?? '';
 $country = $_POST['country'] ?? '';
 $visitedCountries = $_POST['visitedCountries'] ?? '';
 $games = $_POST['games'] ?? '';
+$promise = $_POST['promise'] ?? '';
 
 foreach ($_POST as $key =>$value) {
     $_POST[$key] = htmlspecialchars($value);
@@ -78,10 +81,10 @@ Name:<br>
         echo "<div style='color: red;'>{$errors['name']}</div>";
     }
 ?>
-<input type="text" name="name"><span style='color: red;'>*</span><br><br>
+<input type="text" name="name" value="<?= isset($name) ? $name : '' ?>"><span style='color: red;'>*</span><br><br>
 
 Surname:<br>
-<input type="text" name="surname"><br><br>
+<input type="text" name="surname" value="<?= isset($surname) ? $surname : '' ?>"><br><br>
 
 Email:<br>
 <?php
@@ -89,7 +92,7 @@ Email:<br>
         echo "<div style='color: red;'>{$errors['email']}</div>";
     }
 ?>
-<input type="email"  name="email" ><span style='color: red;'>*</span><br><br>
+<input type="email"  name="email" value="<?= isset($email) ? $email : '' ?>"><span style='color: red;'>*</span><br><br>
 
 Password:<br>
 <?php
@@ -100,7 +103,7 @@ Password:<br>
         echo "<span style='color: red;'>{$errors['pwdCont']}</span><br>";
     }
 ?>
-<input type="password"  name="password"><span style='color: red;'>*</span><br><br>
+<input type="password"  name="password" value="<?= isset($password) ? $password : '' ?>"><span style='color: red;'>*</span><br><br>
 
 Repeat password:<br>
 <?php
@@ -114,10 +117,10 @@ Repeat password:<br>
         echo "<span style='color: red;'>{$errors['pwdMis']}</span><br>";
     }
 ?>
-<input type="password" name="repeatPassword"><span style='color: red;'>*</span><br><br>
+<input type="password" name="repeatPassword" value="<?= isset($repeatPassword) ? $repeatPassword : '' ?>"><span style='color: red;'>*</span><br><br>
 
 About me:<br>
-<textarea name="aboutMe"  value="aboutMe:"></textarea><br><br>
+<textarea name="aboutMe" value="<?= isset($aboutMe0) ? $aboutMe : '' ?>"></textarea><br><br>
 
 Age:<br>
 <?php
@@ -128,7 +131,7 @@ Age:<br>
         echo "<span style='color: red;'>{$errors['age']}</span><br>";
     }
 ?>
-<input type="number" name="age"><br><br>
+<input type="number" name="age" value="<?= isset($age) ? $age : '' ?>"><br><br>
 
 My favorite color:<br>
 <?php
@@ -136,7 +139,8 @@ My favorite color:<br>
         echo "<span style='color: red;'>{$errors['color']}</span><br>";
     }
 ?>
-<input type="color" name="color" style="background-color: 
+<input type="color" name="color" value="<?= isset($color) ? $color : '' ?>"
+    style="background-color: 
     <?= ($color ?? '#ffffff'); ?>"> <br><br>
 
 Country of birth:<br>
@@ -150,16 +154,16 @@ Country of birth:<br>
 ?>
 <select name="country">
     <option value="" selected disabled> </option>
-    <option value="AU">Australia</option>
-    <option value="CA">Canada</option>
-    <option value="CZ">Czech</option>
-    <option value="DK">Denmark</option>
-    <option value="FI">Finland</option>
-    <option value="JP">Japan</option>   
-    <option value="PL">Poland</option>
-    <option value="SE">Sweden</option>   
-    <option value="USA">USA</option>
-    <option value="UA">Ukraine</option> 
+    <option value="AU"<?= ($country === 'AU') ? 'selected' : '' ?>>Australia</option>
+    <option value="CA"<?= ($country === 'CA') ? 'selected' : '' ?>>Canada</option>
+    <option value="CZ"<?= ($country === 'CZ') ? 'selected' : '' ?>>Czech</option>
+    <option value="DK"<?= ($country === 'DK') ? 'selected' : '' ?>>Denmark</option>
+    <option value="FI"<?= ($country === 'FI') ? 'selected' : '' ?>>Finland</option>
+    <option value="JP"<?= ($country === 'JP') ? 'selected' : '' ?>>Japan</option>   
+    <option value="PL"<?= ($country === 'PL') ? 'selected' : '' ?>>Poland</option>
+    <option value="SE"<?= ($country === 'SE') ? 'selected' : '' ?>>Sweden</option>   
+    <option value="USA"<?= ($country === 'USA') ? 'selected' : '' ?>>USA</option>
+    <option value="UA"<?= ($country === 'UA') ? 'selected' : '' ?>>Ukraine</option> 
 </select><span style='color: red;'>*</span><br><br>
 
 Visited countries:<br>
@@ -168,7 +172,7 @@ Visited countries:<br>
     echo "<span style='color: red;'>{$errors['visitedCountries']}</span><br>";
     }
 ?>
-<select name="visitedCountries" multiple>
+<select name="visitedCountries[]" multiple>
     <option value="AU">Australia</option>
     <option value="CA">Canada</option>
     <option value="CZ">Czech</option>
@@ -177,24 +181,24 @@ Visited countries:<br>
     <option value="JP">Japan</option>   
     <option value="PL">Poland</option>
     <option value="SE">Sweden</option>   
-    <option value="USA">USA</option>
+    <option value="AU">USA</option>
     <option value="UA">Ukraine</option> 
 </select><br><br>
 
 How often do you play computer games:<br>
-<input type="radio" name="games" value="Every day">
+<input type="radio" name="games" value="Every day" <?= ($games === 'Every day') ? 'checked' : '' ?>>
 <label for="option1">Every day</label>
-<input type="radio" name="games" value="Once a week">
+<input type="radio" name="games" value="Once a week" <?= ($games === 'Once a week') ? 'checked' : '' ?>>
 <label for="option2">Once a week</label>
-<input type="radio" name="games" value="Once a month">
+<input type="radio" name="games" value="Once a month" <?= ($games === 'Once a month') ? 'checked' : '' ?>>
 <label for="option3">Once a month</label>
-<input type="radio" name="games" value="Rarely">
+<input type="radio" name="games" value="Rarely" <?= ($games === 'Rarely') ? 'checked' : '' ?>>
 <label for="option4">Rarely</label>
-<input type="radio" name="games" value="Never">
+<input type="radio" name="games" value="Never" <?= ($games === 'Never') ? 'checked' : '' ?>>
 <label for="option5">Never</label><br><br>
 
 I promise I'll be a good girl:
-<input type="checkbox" name="promise" value="yes"><br><br>
+<input type="checkbox" name="promise" value="yes"<?= ($promise === 'yes') ? 'checked' : '' ?>><br><br>
 
 <button type="submit" name="submit" value="submit">Submit</button>
 
